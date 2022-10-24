3b1b3242885c0314e081ec1950928a8a5c3b0dd2
jieye/mbp <jieye.ericx@gmail.com>
2022-10-25 01:37:26 +0800

    fix:修改版本号

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Trainify-proto",
-    version="0.0.4",
+    version="0.0.5",
     author="ericx",
     author_email="ericxlee@formail.com",
     description="Trainify-proto",
