215cb5773378ce4729450bc2017d228fcd2ee933
李杪迪 <1246345129@qq.com>
2022-10-27 12:51:14 +0800

    采用cfg配置文件自定义actor网络参数

--- trainify/agent/ddpg_agent.py
+++ trainify/agent/ddpg_agent.py
@@ -7,26 +7,79 @@ import torch.nn.functional as F
 import torch.optim as optim
 from trainify.utils import str_to_list
 
+def parse_cfg(cfgfile):
+    """
+    解析cfg文件
+
+    """
+
+    file = open(cfgfile, 'r')
+    lines = file.read().split('\n')  # store the lines in a list
+    lines = [x for x in lines if len(x) > 0]  # get read of the empty lines
+    lines = [x for x in lines if x[0] != '#']  # get rid of comments
+    lines = [x.rstrip().lstrip() for x in lines]  # get rid of fringe whitespaces
+
+    block = {}
+    blocks = []
+
+    for line in lines:
+        if line[0] == "[":  # This marks the start of a new block
+            if len(block) != 0:  # If block is not empty, implies it is storing values of previous block.
+                blocks.append(block)  # add it the blocks list
+                block = {}  # re-init the block
+            block["type"] = line[1:-1].rstrip()  # remove the fisrt and last element
+        else:
+            key, value = line.split("=")
+            block[key.rstrip()] = value.lstrip()
+    blocks.append(block)
+    return blocks
+
+def create_modules(blocks):
+    """
+    根据解析cfg文件的结果来构造模块
+    :param blocks:
+    :return:
+    """
+    module = nn.Sequential()
+
+    for index, x in enumerate(blocks[:]):
+
+        # If it's a linear layer
+        if (x["type"] == "linear"):
+            # Get the info about the layer
+
+            in_features = int(x["in_features"])
+            out_features = int(x["out_features"])
+            mean = float(x["mean"])
+            std = float(x["std"])
+            bias_zero = bool(x["bias_zero"])
+            activation = x["activation"]
+
+
+            # Add the linear layer
+            linear = nn.Linear(in_features=in_features, out_features=out_features)
+            linear.weight.data.normal_(mean, std)
+            if bias_zero:
+                linear.bias.data.zero_()
+
+            module.add_module("linear_{0}".format(index), linear)
+
+            # activation
+            if activation == "tanh":
+                act = nn.Tanh()
+                module.add_module("tanh_{0}".format(index), act)
+
+
+    return module
 
 class Actor(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, cfgfile):
         super(Actor, self).__init__()
-        self.linear1 = nn.Linear(input_size, hidden_size)
-        self.linear1.weight.data.normal_(0, 0.1)
-        self.linear1.bias.data.zero_()
-        self.linear2 = nn.Linear(hidden_size, hidden_size)
-        self.linear2.weight.data.normal_(0, 0.1)
-        self.linear2.bias.data.zero_()
-        self.linear3 = nn.Linear(hidden_size, output_size)
-        self.linear3.weight.data.normal_(0, 0.1)
-        self.linear3.bias.data.zero_()
+        self.blocks = parse_cfg(cfgfile)
+        self.module_sequential = create_modules(self.blocks)
 
-    def forward(self, s):
-        x = torch.tanh(self.linear1(s))
-        x = torch.tanh(self.linear2(x))
-        x = self.linear3(x)
-
-        return x
+    def forward(self, x):
+        return self.module_sequential(x)
 
 
 class Critic(nn.Module):
@@ -144,3 +197,7 @@ class DDPGAgent(object):
         actor_learn()
         soft_update(self.critic_target, self.critic, self.tau)
         soft_update(self.actor_target, self.actor, self.tau)
+
+if __name__ == '__main__':
+    act = Actor("./cfg/actor.cfg")
+    print(act)
