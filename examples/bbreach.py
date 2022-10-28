beae281be51e5f9de931474f66311266c63e154d
jieye/mbp <jieye.ericx@gmail.com>
2022-10-28 20:33:25 +0800

    更新版本号

--- examples/bbreach.py
+++ examples/bbreach.py
@@ -11,87 +11,14 @@ from trainify.env.BBReach.b4 import B4Env
 from evaluate.evaluate_pendulum import evaluate_pendulum
 
 if __name__ == "__main__":
-    # env_config = {
-    #     "dim": 3,
-    #     "states_name": ['x1', 'x2', 'x3'],
-    #     "state_space": [[-2.5, -2.5, -2.5], [2.5, 2.5, 2.5]],
-    #     "abs_initial_intervals": [0.5, 0.5, 0.5],
-    #     "state_key_dim": [0, 1],
-    #     "dynamics": ['x[0] + (-x[0] + x[1] - x[2]) * 0.02', 'x[1] + (-x[0] * (x[2] + 1) - x[1]) * 0.02',
-    #                  'x[2] + (-x[0] + action) * 0.02'],
-    # }
-    #
-    # agent_config = {
-    #     'gamma': 0.99,
-    #     'actor_lr': 0.0001,
-    #     'critic_lr': 0.001,
-    #     'tau': 0.02,
-    #     'capacity': 10000,
-    #     'batch_size': 32,
-    #     'hidden_size': 256,
-    #     'models_need_save': ['actor', 'critic', 'actor_target', 'critic_target'],
-    #     "modules": [
-    #         {
-    #             "type": "linear",
-    #             "in_features": 6,
-    #             "out_features": 20,
-    #             "mean": 0,
-    #             "std": 0.1,
-    #             "bias_zero": True,
-    #             "activation": "tanh",
-    #         },
-    #         {
-    #             "type": "linear",
-    #             "in_features": 20,
-    #             "out_features": 20,
-    #             "mean": 0,
-    #             "std": 0.1,
-    #             "bias_zero": True,
-    #             "activation": "tanh",
-    #         },
-    #         {
-    #             "type": "linear",
-    #             "in_features": 20,
-    #             "out_features": 1,
-    #             "mean": 0,
-    #             "std": 0.1,
-    #             "bias_zero": True,
-    #             "activation": 'tanh',
-    #         },
-    #     ]
-    # }
-    #
-    # verify_config = {
-    #     "distance_threshold": [0.001, 0.0001, 0.0001],
-    #     "initial_set": [0.25, 0.08, 0.25, 0.27, 0.1, 0.27],
-    #     "max_step": 35,
-    #     "initial_set_partition": [0.01, 0.01, 0.02]
-    # }
-    #
-    # train_config = {
-    #     'step_num': 500,
-    #     'episode_num': 2000
-    # }
-    #
-    # t = Trainify(
-    #     env_config=env_config,
-    #     env_class=Pendulum,
-    #     agent_config=agent_config,
-    #     agent_class=DDPGAgent,
-    #     verify=True,
-    #     verify_config=verify_config,
-    #     experiment_name="test_ddpg_pendulum",
-    # )
-    # t.do_BBreach()
-    # t.train_agent(train_config)
-
     env_config = {
-        "dim": 2,
-        "states_name": ['x1', 'x2'],
-        "state_space": [[-2.5, -2.5], [2.5, 2.5]],
-        "abs_initial_intervals": [0.05, 0.05],
+        "dim": 3,
+        "states_name": ['x1', 'x2', 'x3'],
+        "state_space": [[-2.5, -2.5, -2.5], [2.5, 2.5, 2.5]],
+        "abs_initial_intervals": [0.5, 0.5, 0.5],
         "state_key_dim": [0, 1],
-        "dynamics": ['x[0] + x[1] * 0.1', 'x[1] + (action*x[1]*x[1]-x[0]) * 0.1'],
+        "dynamics": ['x[0] + (-x[0] + x[1] - x[2]) * 0.02', 'x[1] + (-x[0] * (x[2] + 1) - x[1]) * 0.02',
+                     'x[2] + (-x[0] + action) * 0.02'],
     }
 
     agent_config = {
@@ -102,14 +29,14 @@ if __name__ == "__main__":
         'capacity': 10000,
         'batch_size': 32,
         'hidden_size': 256,
-        'models_need_save': ['actor'],
+        'models_need_save': ['actor', 'critic', 'actor_target', 'critic_target'],
         "modules": [
             {
                 "type": "linear",
-                "in_features": 4,
+                "in_features": 6,
                 "out_features": 20,
                 "mean": 0,
-                "std": 1,
+                "std": 0.1,
                 "bias_zero": True,
                 "activation": "tanh",
             },
@@ -118,7 +45,7 @@ if __name__ == "__main__":
                 "in_features": 20,
                 "out_features": 20,
                 "mean": 0,
-                "std": 1,
+                "std": 0.1,
                 "bias_zero": True,
                 "activation": "tanh",
             },
@@ -127,7 +54,7 @@ if __name__ == "__main__":
                 "in_features": 20,
                 "out_features": 1,
                 "mean": 0,
-                "std": 1,
+                "std": 0.1,
                 "bias_zero": True,
                 "activation": 'tanh',
             },
@@ -135,19 +62,17 @@ if __name__ == "__main__":
     }
 
     verify_config = {
-        "distance_threshold": [0.001, 0.001],
-        "initial_set": [0.8, 0.5, 0.9, 0.6],
-        "max_step": 20,
-        "initial_set_partition": [0.01, 0.1]
+        "distance_threshold": [0.001, 0.0001, 0.0001],
+        "initial_set": [0.25, 0.08, 0.25, 0.27, 0.1, 0.27],
+        "max_step": 35,
+        "initial_set_partition": [0.01, 0.01, 0.02]
     }
 
     train_config = {
-        'step_num': 150,
+        'step_num': 500,
         'episode_num': 2000,
         'reward_threshold': 470
     }
-    ROOT_PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    MODEL_PATH = os.path.join(ROOT_PROJECT_PATH, 'trainify/BBReach')
 
     t = Trainify(
         env_config=env_config,
@@ -157,8 +82,6 @@ if __name__ == "__main__":
         verify=False,
         verify_config=verify_config,
         experiment_name="test_b4",
-        log_path='/Users/ericx/PycharmProjects/Trainify-proto/data/test_b4_20221028_195530'
     )
-    # t.load_model()
-    # t.train_agent(train_config)
+    t.train_agent(train_config)
     t.do_BBreach()
